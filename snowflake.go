a5adab490addefa22f0674c0ddb0f76b5663f7e1
smartwalle <smartwalle@gmail.com>
2019-03-07 12:00:17 +0800

    update

--- snowflake.go
+++ snowflake.go
@@ -10,15 +10,18 @@ import (
 const (
 	kSequenceBits   uint8 = 12 // 序列号占用的位数, 表示每个集群下的每个节点1毫秒内可生成的 id 序号的二进制位, 即每毫秒可生成 2^12-1=4096 个唯一 id
 	kDataCenterBits uint8 = 5  // 数据中心占用的位数
-	kWorkerBits     uint8 = 5  // 机器标识占用的位数
+	kMachineBits    uint8 = 5  // 机器标识占用的位数
 
 	kMaxSequence   int64 = -1 ^ (-1 << kSequenceBits)   // 序列号最大值，用于防止溢出   0-4095
 	kMaxDataCenter int64 = -1 ^ (-1 << kDataCenterBits) // 数据中心最大值，用于防止溢出 0-31
-	kMaxWorker     int64 = -1 ^ (-1 << kWorkerBits)     // 机器标识最大值，用于防止溢出 0-31
+	kMaxMachine    int64 = -1 ^ (-1 << kMachineBits)    // 机器标识最大值，用于防止溢出 0-31
 
-	kTimeShift       = kDataCenterBits + kWorkerBits + kSequenceBits // 时间戳向左的偏移量
-	kDataCenterShift = kWorkerBits + kSequenceBits                   // 数据中心向左的偏移量
-	kMachineShift    = kSequenceBits                                 // 机器标识向左的偏移量
+	kTimeShift       = kDataCenterBits + kMachineBits + kSequenceBits // 时间戳向左的偏移量
+	kDataCenterShift = kMachineBits + kSequenceBits                   // 数据中心向左的偏移量
+	kMachineShift    = kSequenceBits                                  // 机器标识向左的偏移量
+
+	kDataCenterMask = kMaxDataCenter << kDataCenterShift
+	kMachineMask    = kMaxMachine << kSequenceBits
 )
 
 // --------------------------------------------------------------------------------
@@ -44,8 +47,8 @@ func WithDataCenter(dataCenter int64) Option {
 
 func WithMachine(machine int64) Option {
 	return optionFunc(func(s *SnowFlake) error {
-		if machine < 0 || machine > kMaxWorker {
-			return errors.New(fmt.Sprintf("worker can't be greater than %d or less than 0", kMaxWorker))
+		if machine < 0 || machine > kMaxMachine {
+			return errors.New(fmt.Sprintf("worker can't be greater than %d or less than 0", kMaxMachine))
 		}
 		s.machine = machine
 		return nil
@@ -123,6 +126,27 @@ func (this *SnowFlake) getNextMillisecond() int64 {
 
 func (this *SnowFlake) getMillisecond() int64 {
 	return time.Now().UnixNano() / 1e6
+}
+
+// --------------------------------------------------------------------------------
+// Time 获取 id 的时间差值，单位是 millisecond
+func Time(s int64) int64 {
+	return s >> kTimeShift
+}
+
+// DataCenter 获取 id 的数据中心标识
+func DataCenter(s int64) int64 {
+	return s & kDataCenterMask >> kDataCenterShift
+}
+
+// Machine 获取 id 的机器标识
+func Machine(s int64) int64 {
+	return s & kMachineMask >> kMachineShift
+}
+
+//  Sequence 获取 id 的序列号
+func Sequence(s int64) int64 {
+	return s & kMaxSequence
 }
 
 // --------------------------------------------------------------------------------
